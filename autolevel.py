6c8c03ad165737a0ab16d93c858cb10e693288c0
unique-pear486 <unique-pear486@users.noreply.github.com>
2015-10-15 22:33:39 +1100

    First pass generation

--- autolevel.py
+++ autolevel.py
@@ -4,6 +4,13 @@ import random
 import numpy as np
 
 
+DIRECTIONS = (np.array((0, 1)),
+              np.array((1, 0)),
+              np.array((0, -1)),
+              np.array((-1, 0)))     # Cardinal directions
+WINDINGNESS = 20        # 1/WINDINESS chance of turning without being forced
+
+
 class Room(object):
     """A room object. Occupys cells x1 to x2-1, y1 to y2-1"""
     def __init__(self, x1, y1, x2, y2):
@@ -29,34 +36,22 @@ class Room(object):
         return (self.x1, self.y1, self.x2, self.y2)
 
 
-def generate_dungeon(x, y, height, **options):
+def generate_dungeon(x, y, height, attempts, **options):
     """Generate a dungeon x by y cells, height cells high"""
+    if (x % 2 != 1) or (y % 2 != 1):
+        yield ValueError("Sizes must be odd")
     terrain = np.zeros([x, y], dtype=int)
-    rooms = generate_rooms(x, y)
+    rooms = generate_rooms(x, y, 200)
     yield rooms
+    region = 1
     for room in rooms:
-        carve(room, terrain)
-    yield net_from_points([r.centre for r in rooms],
-                          Triangle((0, 0), (0, x+y), (x+y, 0)))
-    net = net_from_rooms(rooms, Triangle((0, 0), (0, x+y), (x+y, 0)))
-    for l in net:
-        if (l.p2.x1 < l.p1.x2) and (l.p2.x2 > l.p1.x1):
-            corr_x = random.randint(max(l.p1.x1, l.p2.x1),
-                                    min(l.p1.x2, l.p2.x2))
-            carve(Room(corr_x, l.p1.centre[1], corr_x+1, l.p2.centre[1]),
-                  terrain)
-        elif (l.p2.y1 < l.p1.y2) and (l.p2.y2 > l.p1.y1):
-            corr_y = random.randint(max(l.p1.y1, l.p2.y1),
-                                    min(l.p1.y2, l.p2.y2))
-            carve(Room(l.p1.centre[0], corr_y, l.p2.centre[0], corr_y+1),
-                  terrain)
-        else:
-            corr_x = random.randint(l.p1.x1, l.p1.x2)
-            corr_y = random.randint(l.p2.y1, l.p2.y2)
-            carve(Room(corr_x, l.p1.centre[1], (corr_x + 1), corr_y+1),
-                  terrain)
-            carve(Room(corr_x, corr_y, l.p2.centre[0], (corr_y + 1)),
-                  terrain)
+        carve(room, terrain, region)
+        region += 1
+    for i in range(1, terrain.shape[0], 2):
+        for j in range(1, terrain.shape[1], 2):
+            if terrain[i, j] == 0:
+                grow_maze(terrain, (i, j), region)
+                region += 1
     yield terrain
     # terr_temp = np.zeros([x+2, y+2], dtype=int)
     # terr_temp[1:-1, 1:-1] = terrain
@@ -67,34 +62,27 @@ def generate_dungeon(x, y, height, **options):
     yield terrain + 2*N_walls + 2*S_walls + 2*E_walls + 2*W_walls
 
 
-def generate_rooms(x, y):
+def generate_rooms(x, y, tries):
     """Generate dungeon rooms x by y cells"""
     rooms = []
-    for i in range(0, 20):
-        added = False
-        i = 0
-        while added is False and i < 100:
-            i += 1
-            x1 = random.randint(1, x-1)
-            x2 = random.randint(1, x-1)
-            y1 = random.randint(1, y-1)
-            y2 = random.randint(1, y-1)
-            if abs(x1 - x2) < 4:
-                continue
-            if abs(y1 - y2) < 4:
-                continue
-            ratio = abs((x1-x2) / (y1-y2))
-            if ratio < 0.2 or ratio > 5:
-                continue
-            room = Room(x1, y1, x2, y2)
-            r = random.random()**4
-            if room.area > r * x * y:
-                continue
-            if any(intersects(room, r) for r in rooms):
-                continue
-            else:
-                rooms.append(room)
-                added = True
+    maxsize = min(x, y) // 2
+    for i in range(0, tries):
+        x1 = random.randint(1, x // 2)*2 + 1
+        y1 = random.randint(1, y // 2)*2 + 1
+        width = int(random.random()**2 * maxsize) + 4
+        height = int(random.random()**2 * maxsize) + 4
+        ratio = abs(width / height)
+        if ratio < 0.1 or ratio > 10:
+            continue
+        x2 = x1 + width
+        y2 = y1 + width
+        if x2 >= x or y2 >= y:
+            continue
+        room = Room(x1, y1, x2, y2)
+        if any(intersects(room, r) for r in rooms):
+            continue
+        else:
+            rooms.append(room)
     return rooms
 
 
@@ -108,10 +96,51 @@ def intersects(room1, room2):
         return False
 
 
-def carve(room, terrain, n=1):
-    for i in range(room.x1, room.x2):
-        for j in range(room.y1, room.y2):
-            terrain[j][i] = n
+def carve(area, terrain, n=1):
+    if hasattr(area, "x1"):
+        for i in range(area.x1, area.x2):
+            for j in range(area.y1, area.y2):
+                terrain[i, j] = n
+    else:
+        terrain[tuple(area)] = n
+
+
+def grow_maze(terrain, start, region):
+    cells = [start]
+    previous_direction = random.sample(DIRECTIONS, 1)[0]
+    while cells:
+        cell = cells[-1]
+        open_cells = []
+        # Search for direcions we can carve
+        for direction in DIRECTIONS:
+            if can_carve(cell, direction, terrain):
+                open_cells.append(direction)
+
+        # Depending on Winding factor, prefer previous direction
+        if open_cells:
+            if (tuple(previous_direction) in [tuple(i) for i in open_cells] and
+                    random.random() > 1/WINDINGNESS):   # tuple for np wierdnes
+                direction = previous_direction
+            else:
+                direction = random.sample(open_cells, 1)[0]
+            carve(cell + direction, terrain, region)
+            carve(cell + direction*2, terrain, region)
+            cells.append(cell + direction*2)
+        else:
+            cells.pop()
+    return terrain
+
+
+def can_carve(cell, direction, terrain):
+    """Check if there are any walls or the boundary in the way"""
+    (x, y) = cell + direction*3
+    if x < 0 or x >= terrain.shape[0]:
+        return False
+    if y < 0 or y >= terrain.shape[1]:
+        return False
+    if terrain[tuple(cell + direction*2)] == 0:
+        return True
+    return False
 
 
 class Triangle(object):
@@ -177,52 +206,6 @@ class Line(object):
         return self.__hash
 
 
-def net_from_points(points, outer_triangle):
-    """Generates a net of connections from a list of points"""
-    i_points = points[:]
-    triangles = [outer_triangle]
-    while i_points:
-        point = i_points.pop(random.randint(0, len(i_points) - 1))
-        rebuild_triangles = []
-        for tri in triangles:
-            if tri.in_circumcircle(point):
-                rebuild_triangles.append(tri)
-        polygon = set([])
-        for i, tri in enumerate(rebuild_triangles):
-            for line in tri:
-                if sum([line in tri2 for tri2 in rebuild_triangles]) > 1:
-                    continue
-                polygon.add(line)
-            triangles.remove(tri)
-        for line in polygon:
-            triangles.append(Triangle(line.p1, line.p2, point))
-    net = set([])
-    outer_points = [outer_triangle.p1, outer_triangle.p2, outer_triangle.p3]
-    for tri in triangles:
-        for l in tri:
-            if l.p1 in outer_points or l.p2 in outer_points:
-                continue
-            net.add(l)
-    return net
-
-
-def net_from_rooms(rooms, outer_triangle):
-    net = net_from_points([room.centre for room in rooms], outer_triangle)
-    room_net = []
-    for line in net:
-        r1 = None
-        r2 = None
-        for r in rooms:
-            if r.centre == line.p1:
-                r1 = r
-            if r.centre == line.p2:
-                r2 = r
-        if r1 is None or r2 is None:
-            raise TypeError
-        room_net.append(Line(r1, r2))
-    return room_net
-
-
 if __name__ == "__main__":
     import Image
     import ImageDraw
@@ -233,22 +216,20 @@ if __name__ == "__main__":
     plt.ion()
     plt.show()
     # full procedure
-    gen_dun = generate_dungeon(100, 100, 3)
+    gen_dun = generate_dungeon(21, 21, 3, 200)
     rooms = gen_dun.send(None)
-    net = gen_dun.send(None)
     a = Image.new("RGBA", (100, 100), (255, 255, 255))
     draw = ImageDraw.Draw(a)
     for room in rooms:
         draw.rectangle(room.coords, outline=(0, 0, 0))
-    for line in net:
-        draw.line((line.p1, line.p2), fill=(255, 0, 0))
     plt.imshow(a, interpolation="nearest")
     plt.draw()
     time.sleep(1)
     terr = gen_dun.send(None)
-    plt.imshow(terr, interpolation="nearest", cmap=cm.gray, origin="lower")
+    plt.imshow(terr, interpolation="nearest", cmap=cm.gray, origin="lower",
+               vmin=0, vmax=1)
     plt.draw()
-    time.sleep(2)
+    time.sleep(5)
     terr2 = gen_dun.send(None)
     plt.imshow(terr2, interpolation="nearest", cmap=cm.gray, origin="lower")
     plt.draw()
